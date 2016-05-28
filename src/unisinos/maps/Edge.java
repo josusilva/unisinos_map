c6aa42747713814b15a1ddec0fa9845b627a3b06
josue <josue@josue-K45VM>
2016-05-27 21:54:23 -0300

    criado as classes edge e vertex

---
+++ src/unisinos/maps/Edge.java
@@ -0,0 +1,14 @@
+package unisinos.maps;
+
+public class Edge {
+	private Vertex origin;
+	private Vertex destiny;
+	
+	public Edge(Vertex origin, Vertex destiny) {
+		this.origin = origin;
+		this.destiny = destiny;
+	}
+	
+	
+
+}
