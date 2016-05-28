5b79fb7b85409c805d7f8b65074e436b7548da12
josue <josue@josue-K45VM>
2016-05-27 21:54:47 -0300

    criado as classes edge e vertex

---
+++ src/unisinos/maps/Vertex.java
@@ -0,0 +1,21 @@
+package unisinos.maps;
+
+import java.util.ArrayList;
+
+public class Vertex<E> {
+	
+	private E element;
+	
+	private ArrayList<Edge> adjacency;
+	
+	public Vertex(E element) {
+		this.element = element;
+		this.adjacency = new ArrayList<Edge>();
+	}
+		
+	public void addEdge(Edge edge){
+		adjacency.add(edge);
+	}
+
+
+}
